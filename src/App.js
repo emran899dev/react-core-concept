0f641116476e2fe03041b35f3ff71e3648637edc
Md Emran Nazir <mdemrannazir9999@gmail.com>
2020-08-22 08:44:50 +0600

    multipul tag using component

--- src/App.js
+++ src/App.js
@@ -5,16 +5,24 @@ import './App.css';
 function App() {
   return (
     <header className="App-header">
-    <div className="App">
+      <div className="App">
         <p>I am a React Person</p>
         <Person></Person>
+        <Person></Person>
+        <Person></Person>
       </div>
-      </header>
+    </header>
   );
 }
 
 function Person() {
-  return <h1>Name: Mushfiqur Rahim</h1>
+  return (
+    <div>
+      <h1>Name: Mushfiqur Rahim</h1>
+      <h3>I am a student</h3>
+    </div>
+  )
+
 }
 
 export default App;
