9512a04616701be4d4989bb1bc3f7bcbfd73e07b
Md Emran Nazir <mdemrannazir9999@gmail.com>
2020-08-22 22:29:17 +0600

    use direkli setCount method

--- src/App.js
+++ src/App.js
@@ -116,9 +116,8 @@ function Counter() {
   return (
     <div>
       <h2>Count: {count} </h2>
-
-      <button onClick={handelIncrease}>Increase</button>
-      <button onClick={handelDecrease}>Decrease</button>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
     </div>
   )
 }
